c7640dd1440a288fe5b4020031d4731e97f83d1f
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-12 17:24:56 +0300

    Adds elements to ExpenseTable.jsx

--- src/components/Table/ExpenseTable.jsx
+++ src/components/Table/ExpenseTable.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import "./ExpenseTable.css";
 
-export default function ExpenseTable() {
-  return <div>ExpenseTable</div>;
+export default function ExpenseTable({ data }) {
+  return (
+    <div>
+      <h2>Submitted info:</h2>
+      {data ? (
+        <div>
+          <p>
+            <strong>Name:</strong>
+            {data.name}
+          </p>
+          <p>
+            <strong>Description:</strong>
+            {data.description}
+          </p>
+          <p>
+            <strong>Category:</strong>
+            {data.category}
+          </p>
+          <p>
+            <strong>Amount:</strong>
+            {data.amount}
+          </p>
+        </div>
+      ) : (
+        <p>No data submitted yet</p>
+      )}
+    </div>
+  );
 }
